8632ba4d5c6863f51f292ce8cf1aca96b256eb4b
Victor Rubino <rubinodev@gmail.com>
2022-09-20 11:58:25 -0300

    formatted values

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { TransactionsContext, TransactionsProvider } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formater";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
@@ -22,11 +23,12 @@ export function Transactions(){
                 <td width="50%">{transaction.description}</td>
                 <td>
                   <PriceHighlight variant={transaction.type}>
-                    {transaction.price}
+                    {transaction.type === 'outcome' && '- '}
+                    {priceFormatter.format(transaction.price)}
                   </PriceHighlight>
                 </td>
                 <td>{transaction.category}</td>
-                <td>{transaction.createdAt}</td>
+                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
               </tr> 
             ))}
           </tbody>
