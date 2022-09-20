b9eddb7df333f165d7c04b8f98b05a2d08079714
Victor Rubino <rubinodev@gmail.com>
2022-09-20 11:44:31 -0300

    summary reduce and transactions context

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,13 @@
+import { useContext, useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { TransactionsContext, TransactionsProvider } from "../../contexts/TransactionsContext";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions(){
+  const { transactions } = useContext(TransactionsContext);
+
   return(
     <div>
       <Header/>
@@ -13,28 +17,18 @@ export function Transactions(){
         <SearchForm/>
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
-                <PriceHighlight variant="income">
-                  R$ 17.000,00
-                </PriceHighlight>
-              </td>
-              <td>Venda</td>
-              <td>15/09/2022</td>
-            </tr> 
-
-            <tr>
-              <td width="50%">Hamburguer</td>
-              <td>
-                <PriceHighlight variant="outcome">
-                  - R$ 17.000,00
-                </PriceHighlight>
-              </td>
-              <td>Alimentos</td>
-              <td>15/09/2022</td>
-            </tr>
-
+            {transactions.map(transaction => (
+              <tr key={transaction.id}>
+                <td width="50%">{transaction.description}</td>
+                <td>
+                  <PriceHighlight variant={transaction.type}>
+                    {transaction.price}
+                  </PriceHighlight>
+                </td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr> 
+            ))}
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
