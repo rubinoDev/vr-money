db35edd2cb70568286f41b1408ec412a27408794
Victor Rubino <rubinodev@gmail.com>
2022-09-26 22:35:51 -0300

    adding resolvers

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,10 +1,31 @@
 import { MagnifyingGlass } from "phosphor-react";
+import { useForm } from "react-hook-form";
 import { SearchFormContainer } from "./styles";
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod' 
+
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm(){
+  const { register, handleSubmit } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema)
+  });
+
+  function handleSearchTransactions(data: SearchFormInputs){
+    console.log('all right')
+  }
+
   return(
-    <SearchFormContainer>
-      <input type="text" placeholder="Busque por transações"/>
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input 
+        type="text" 
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
 
       <button type="submit">
         <MagnifyingGlass size={20}/>
